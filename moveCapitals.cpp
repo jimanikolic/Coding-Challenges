06c72f41fd36df15b23dab39b8a12edd0776f35f
jimanikolic <jimanikolic@gmail.com>
2024-04-27 12:25:35 -0400

    bubble sort in moveCapitals

--- moveCapitals.cpp
+++ moveCapitals.cpp
@@ -9,9 +9,18 @@ int main () {
     string myString;
     myString = "TeSt Me PlEaSe";
 
-    for (int i = 0; i < myString.length(); i++) {
-        cout << myString[i];
-    }
+    int n = myString.length();
 
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n -1; j++) {
+            if (myString[j] > myString[j+1]) {
+                char temp = myString[j];
+                myString[j] = myString[j+1];
+                myString[j+1] = temp;
+            }
+        }
+    }
+    
+    cout << myString;
 }
 
