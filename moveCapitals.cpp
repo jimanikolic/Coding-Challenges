00befcc57c33ad083fc06f48ed82ecabf55dc524
jimanikolic <jimanikolic@gmail.com>
2024-04-27 12:38:14 -0400

    optimized buble sort with -i

--- moveCapitals.cpp
+++ moveCapitals.cpp
@@ -1,3 +1,8 @@
+/*
+Challenge #1 - Create a function that moves all capital letters to the front of a word.
+Runtime - O(n^2), where n is the length of the string;
+Solution - Simple bubble sort acting on ascii values
+*/
 #include <iostream>
 #include <string>
 
@@ -8,11 +13,16 @@ int main () {
 
     string myString;
     myString = "TeSt Me PlEaSe";
-
+    // n is the total number of elemetns in the array
     int n = myString.length();
 
+    // simple Bubble sort, ascii value will pull out capital letters
+    // i is number of iterations so far
     for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n -1; j++) {
+
+        // after each iteration, largest element is placed at end of array,
+        // so we use -i in the conditional, as we dont need to check end values again
+        for (int j = 0; j < n - i -1; j++) {
             if (myString[j] > myString[j+1]) {
                 char temp = myString[j];
                 myString[j] = myString[j+1];
@@ -20,7 +30,7 @@ int main () {
             }
         }
     }
-    
+
     cout << myString;
 }
 
