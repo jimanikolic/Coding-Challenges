53084b5c880d87994751ac7c9b2a1d7f8eec61ca
jimanikolic <jimanikolic@gmail.com>
2024-05-07 15:05:15 -0400

    test case

--- charPointersTest.cpp
+++ charPointersTest.cpp
@@ -1,10 +1,12 @@
-#include <iostream>
+//some c testing data stuff using casting, shows how a string is stored as ascii hex in main mem.
 #include <cstring>
-#include <stdio.h>
+#include <iostream>
+
 typedef unsigned char *byte_pointer;
 
 using namespace std;
 
+//show bytes in hex, use with poitners to see values stored at addresses
 void show_bytes(byte_pointer start, size_t len) {
   int i;
   for (i = 0; i<len; i++) {
@@ -14,8 +16,12 @@ void show_bytes(byte_pointer start, size_t len) {
 }
 
 int main(){
-  const char *m = "mnopqr";
-  show_bytes((byte_pointer) m, strlen(m));
+  short x = 12345;
+  short mx = -x; 
+
+  show_bytes((byte_pointer) &x, sizeof(short));
+  show_bytes((byte_pointer) &mx, sizeof(short));
+
 
 
 
