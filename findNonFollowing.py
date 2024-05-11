32578dcb680aa745bfbc4d29af2d123eeb4e4723
jimanikolic <jimanikolic@gmail.com>
2024-05-11 15:25:08 -0400

    added instructions

--- findNonFollowing.py
+++ findNonFollowing.py
@@ -1,3 +1,37 @@
+'''
+Challenge #8 - Write a examines two csv files from instagram to determine
+who you are following that does not follow you back and vice-versa.
+Runtime O(1) constant
+Python Implementation - honestly better than cpp 
+because instagram throttles data - cant scrape 
+it i tried and got banned... twice
+
+Instructions:
+Download data from instagram
+- On PC
+- settings and privacy 
+- meta accounts center
+- your information and permissions
+- download your information
+- download or transfer information
+- click instagram profile you want data from
+- some of your information
+- followers and following
+- next
+- download to device
+- date range: all time
+- format JSON
+- medium quality
+- create files
+
+Wait until request is aproved - why is this necessary
+Download data from same location, 
+- download or transfer information
+- download
+- unzip
+- move followers.json and following.json into .py directory
+'''
+
 import json
 
 # Function to extract usernames from JSON file
@@ -30,5 +64,3 @@ nonfollowers = [user for user in followers if user not in following]
 print ("\nNames you don't follow back")
 for user in nonfollowers:
     print(user)
-
-
